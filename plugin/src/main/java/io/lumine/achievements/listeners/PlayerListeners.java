fd6ad1276eba532e3b80549f408fe598508faef3
Joshua <39896452+Joshinn-io@users.noreply.github.com>
2023-08-03 11:36:21 -0400

    1.20.1 api bump

--- plugin/src/main/java/io/lumine/achievements/listeners/PlayerListeners.java
+++ plugin/src/main/java/io/lumine/achievements/listeners/PlayerListeners.java
@@ -1,11 +1,6 @@
 package io.lumine.achievements.listeners;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import io.lumine.achievements.MythicAchievementsPlugin;
 
 public class PlayerListeners implements Listener {
 
